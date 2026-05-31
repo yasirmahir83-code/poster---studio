d342d2c5d6a2c9e364b0f112a11e2e2712053d5a
yasirmahir83-code <yasirmahir83@gmail.com>
2026-06-01 00:35:39 +0300

    add server

---
+++ api/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const handler = require('./poster');
+const app = express();
+app.use((req, res) => handler(req, res));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
